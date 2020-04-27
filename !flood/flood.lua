6410d025cdf777fa8ead468bc179467fb5e1dff9
meffo <meffak@wp.pl>
2020-04-28 01:14:51 +0200

    Arbuz ma wode Arbuz ma wode :watermelon:

---
+++ !flood/flood.lua
@@ -0,0 +1,55 @@
+local level = 0
+
+function floodSA(source,command,speed)
+	if (isObjectInACLGroup ("user."..getAccountName(getPlayerAccount(source)),aclGetGroup("Admin"))) then
+		if (speed) then
+			if (isTimer(waterTimer)) then
+				killTimer(waterTimer)
+				destroyElement(floodWater)
+				setWaterLevel(0)
+				level = 0
+				outputDebugString ("Previous flooding has been stopped by the server.")
+			end
+			local speed = tonumber(speed)
+			floodWater = createWater(-2998,-2998,0,2998,-2998,0,-2998,2998,0,2998,2998,0)
+			outputChatBox("--------------------------------",getRootElement(),255,0,0)
+			outputChatBox("UWAGA: STAN ALARMOWY! POZIOM WODY SIE PODNOSI!",getRootElement(),255,0,0)
+			outputChatBox("--------------------------------",getRootElement(),255,0,0)
+			waterTimer = setTimer(addSomeWater,100,0,speed)
+			outputDebugString("Powódź została włączona przez "..getPlayerName (source).." z prędkością "..speed..".")
+		else
+			outputChatBox("Określ prędkość.",source,255,0,0)
+		end
+	else
+		outputChatBox ("Nie posiadasz uprawnień do tej komendy!",source,255,0,0)
+	end
+end
+addCommandHandler("flood",floodSA)
+
+function addSomeWater(speed)
+    level = level+speed/100
+    setWaterLevel(level)
+end
+
+function stopFlood(source,command)
+	if (isObjectInACLGroup("user."..getAccountName(getPlayerAccount(source)),aclGetGroup("Admin"))) then
+			if (isTimer(waterTimer)) then
+				killTimer(waterTimer)
+				destroyElement(floodWater)
+				setWaterLevel(0)
+				level = 0
+				outputDebugString("Flooding stopped by "..getPlayerName(source)..".")
+				outputChatBox("Poziom wody opadł. Sytuacja jest pod kontrolą, możecie wracać!",getRootElement(),255,0,0)
+			else
+				outputChatBox("There is no flood to stop.",source,255,0,0)
+			end
+	else
+		outputChatBox ("Nie posiadasz permisji do tej komendy!",source,255,0,0)
+	end
+end
+addCommandHandler("flood-stop",stopFlood)
+
+function resetWater()
+	setWaterLevel(0)
+end
+addEventHandler("onResourceStop",getResourceRootElement(getThisResource()),resetWater)
